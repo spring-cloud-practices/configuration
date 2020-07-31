7a219ea8a819392492486ddfc02845595b1a0dad
zacscoding <zaccoding725@gmail.com>
2020-07-31 20:18:17 +0900

    README.md 추가

--- config-client/src/main/java/demo/client/RefreshRestController.java
+++ config-client/src/main/java/demo/client/RefreshRestController.java
@@ -3,15 +3,11 @@ package demo.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.endpoint.RefreshEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * See {@link RefreshEndpoint}
- */
 @Slf4j
 @RestController
 @RefreshScope
